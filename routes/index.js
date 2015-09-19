12c1df8d7ead05f700275d00a0554157ed173838
Sunils2991 <sunils2991@gmail.com>
2015-09-19 21:04:59 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -23,16 +23,15 @@ router.post('/issuesDetails', function(req, res){
 
 	/*----Declaring varaibles required in the further process----*/
 	var url = jsonBody.url;				//github project issue url
-				var openIssues = 0, 	//total number of issues
-				openIssues24 = 0, 		// issues in a day
-				openIssues24_7 = 0, 	//issues in a week
-				openIssuesGt24_7 = 0,	//issues above in a week
-				noOfPages; 				//Number of pages
+	var openIssues = 0, 	//total number of issues
+		openIssues24 = 0, 		// issues in a day
+		openIssues24_7 = 0, 	//issues in a week
+		openIssuesGt24_7 = 0,	//issues above in a week
+		noOfPages; 				//Number of pages
 				
     var issueDetails = { openIssues : "", openIssues24 : "", openIssues24_7 : "", openIssuesGt24_7: "" };	//Json data to be sent with issue details
 	
 	var totalIssues = []; 				//
-	var i = 1;							//first page
 	
 	/*----- requesting the github project issues page ---*/
     request(url, function(error, response, html){
@@ -46,28 +45,47 @@ router.post('/issuesDetails', function(req, res){
 			var $ = cheerio.load(html);
 			//class table-list-header-toggle gives the total number of open issues for the project
 			$('.table-list-header-toggle').each(function() {
-					var totalopenIssues1 = $(this).children().first().text();
-
-					totalopenIssues2 = totalopenIssues1.replace(/(\r\n|\n|\r)/gm,"");
-					totalopenIssues = totalopenIssues2.replace(/  +/g, ' ');
-					
-					var openIssuesResult = totalopenIssues.split(" ");
-					openIssues = openIssuesResult[1]; // storing the total number of issues
-					console.log(openIssues);
-					if(1)			//in the "first table-list-header-toggle" class, open issues are found. So breaking the loop 
-						return false;
+				var data = $(this);
+				totalopenIssues1 = data.children().first().text();
+				
+				totalopenIssues2 = totalopenIssues1.replace(/(\r\n|\n|\r)/gm,"");
+				totalopenIssues = totalopenIssues2.replace(/  +/g, ' ');
+				
+				var openIssuesResult = totalopenIssues.split(" ");
+				openIssues = openIssuesResult[1]; // storing the total number of issues
+				if(1)			//in the "first table-list-header-toggle" class, open issues are found. So breaking the loop 
+					return false;
 			});
-			
+				
 			// One page contains 25 issues. If issues are more than 25, request must be made to all pages to get issues date
 			if(openIssues){
-				noOfPages = Math.ceil(openIssues/25);
-				console.log(noOfPages);
+				noOfPages = Math.ceil(parseInt(openIssues)/25);
 			}
 			
-			//Recursive loop helps in synchronized requests
-			(function loop() {
-				if (i <= noOfPages) {   												//Requesting all pages
-					var changedUrl = url+"?page="+i+"&q=is%3Aissue+is%3Aopen";			//URL for requesting all the pages individually	
+			//sending response
+			
+			function sendResponse() {
+				console.log('sendResponse');
+				for(var k = 0; k < totalIssues.length; k++){			// for finding number of issues according to day difference 
+					if (totalIssues[k] < 24 ){ 
+						openIssues24 = openIssues24 + 1 ;
+					}
+					else if (totalIssues[k] <24*7 ){
+						openIssues24_7 = openIssues24_7 + 1;
+					}
+					else 
+					openIssuesGt24_7 = openIssuesGt24_7 + 1;
+				}							
+				issueDetails.openIssues = openIssues;				//forming JSON data
+				issueDetails.openIssues24 = openIssues24;
+				issueDetails.openIssues24_7 = openIssues24_7;
+				issueDetails.openIssuesGt24_7 = openIssuesGt24_7;
+				console.log(issueDetails);
+				res.status(200).json(issueDetails);	
+			}
+			//loop to get all issues date
+			for (i = 1; i <= noOfPages; i++) {
+				var changedUrl = url+"?page="+i+"&q=is%3Aissue+is%3Aopen";			//URL for requesting all the pages individually	
 					request(changedUrl, function(error, response, html){				//requesting thee web page
 						if(error){
 							return console.log(error);
@@ -78,40 +96,23 @@ router.post('/issuesDetails', function(req, res){
 								var dataLoop = $(this);
 								var time = dataLoop.attr('datetime');
 								var issuesDate = new Date(time).getTime();
-								console.log(issuesDate);
 								var today = new Date();									
 								var _MS_PER_DAY = 1000 * 60 * 60;						// for converting milliseconds to hours
 								
 								diffDays = Math.floor((today - issuesDate) / _MS_PER_DAY);			//finding difference hours
 								totalIssues.push(diffDays);									//pushing difference in days data to array
 			
-								if(i == noOfPages && totalIssues.length==openIssues){		// for last request sending response
-									for(var k = 0; k < totalIssues.length; k++){			// for finding number of issues according to day difference 
-										if (totalIssues[k] < 24 ){ 
-											openIssues24 = openIssues24 + 1 ;
-										}
-										else if (totalIssues[k] < (24*7) ){
-											openIssues24_7 = openIssues24_7 + 1;
-										}
-										else 
-										openIssuesGt24_7 = openIssuesGt24_7 + 1;
-									}							
-									issueDetails.openIssues = openIssues;				//forming JSON data
-									issueDetails.openIssues24 = openIssues24;
-									issueDetails.openIssues24_7 = openIssues24_7;
-									issueDetails.openIssuesGt24_7 = openIssuesGt24_7;
-									console.log(issueDetails);
-									res.status(200).json(issueDetails);					// sending response back
+								if(totalIssues.length==openIssues){		// for last request sending response
+									console.log(totalIssues.length);
+									sendResponse();		
 								}								
 							});
 						}
-						i++;
-						loop();
-					});					
-				}
-			}());	
-		}					
-	});	
+					});	
+			}
+		}
+	});
+			
 });
 
 module.exports = router;
